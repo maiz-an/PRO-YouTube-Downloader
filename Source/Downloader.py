ee7fb8bfb8c4553aa68db7289887ba30051046e6
mzieO's <memaizanx@gmail.com>
2025-10-26 23:37:58 +0300

    v2.0

--- Source/Downloader.py
+++ Source/Downloader.py
@@ -20,7 +20,9 @@ if os.name == 'nt':  # Windows
 elif sys.platform == 'darwin':  # macOS
     DEFAULT_DOWNLOAD_DIR = os.path.join(os.path.expanduser("~"), "Downloads", "PRO_Youtube_Downloader")
 else:  # Linux, Android, iOS and others
-    DEFAULT_DOWNLOAD_DIR = os.path.join(os.path.expanduser("~"), "PRO_Youtube_Downloader", "Downloads")
+    # For all other platforms, create app folder in user's home directory
+    # This is safe for iOS sandbox and works well on Linux/Android
+    DEFAULT_DOWNLOAD_DIR = os.path.join(os.path.expanduser("~"), "PRO_Youtube_Downloader")
 
 DOWNLOAD_DIR = DEFAULT_DOWNLOAD_DIR
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
